3a5a631ac33cc83b9ea8ecdda9890b446e902b03
Nguyen Quang Huy <huy460839@gmail.com>
2026-08-23 23:51:53 +0700

    Clean up build files and update gitignore

--- Week-1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ Week-1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146e6bdc9ff9e1976c6f587823b774abe9f8cdd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a889a8c4937bf58c9fd29823f6c2f952f9ef3732")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
